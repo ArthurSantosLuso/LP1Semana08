19e021b3ea70c7732999103cc781fa634127f9d9
Arthur Santos <arthurluiz.developer@gmail.com>
2025-04-10 14:30:12 +0100

    MilitaryUnit.cs alterado.

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -1,14 +1,35 @@
+using System;
+
 namespace GameUnit
 {
-    public class MilitaryUnit : Unit // Heranca!!
+    public class MilitaryUnit : Unit
     {
         public int AttackPower { get; }
+
         public int XP { get; private set; }
 
+        // override da health
+        public override int Health
+        {
+            get => base.Health + XP;
+        }
+
+        // override do cost
+        public override float Cost => AttackPower + XP;
+
+        // class ctor
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
-        {                                                        // Unit(int, int)
+        {
             AttackPower = attackPower;
             XP = 0;
         }
+
+        // Attack method 
+        public void Attack(Unit u)
+        {
+            XP++; // increment self xp
+            u.Health -= AttackPower; // apply damage to the unit
+        }
     }
 }
+
